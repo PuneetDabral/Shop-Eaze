4957a3ac1670a144a2062d2269ee5c40642cba21
Puneet Dabral <puneetdabral2000@gmail.com>
2022-01-11 21:51:55 +0530

    project done

---
+++ admin/connection.inc.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$con=mysqli_connect("localhost","root","","ecom");
+// define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/php/ecom/');
+// define('SITE_PATH','http://127.0.0.1/php/ecom/');
+
+// define(''../media/product/'',SERVER_PATH.''../media/product/'');
+// define(''../media/product/'',SITE_PATH.''../media/product/'');
+?>
